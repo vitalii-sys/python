1c6a45671567ea3a13ec4c8e6df3c6a698143b26
Vitalii Trefilov <fr2k@mail.ru>
2020-01-17 12:21:05 +0500

    1

--- lesson2/Task4.py
+++
@@ -1,5 +0,0 @@
-string = input('Введите несколько слов через пробел: ')
-words = string.split()
-
-for i, word in enumerate(words, start=1):
-    print(i, word[:10])
--- lesson3/Task6.py
+++
@@ -1,6 +0,0 @@
-def int_func(text):
-    return text.title()
-
-
-print(int_func('text2'))
-print(int_func('text txt more more text'))
